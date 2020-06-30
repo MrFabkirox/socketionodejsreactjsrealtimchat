bd39f5c40e8f5f18d0a549aaa964125d409f2de2
mrfabkirox <fabrice.esope@gmail.com>
2020-06-30 08:42:40 +0800

    getting icones

--- server/index.js
+++ server/index.js
@@ -11,7 +11,24 @@ const PORT  = process.env.PORT  || 5000;
 const router = require('./router');
 
 const app = express();
-app.use(cors())
+// app.use(cors())
+
+// // handling cors error
+// app.use((req, res, next) => {
+//   res.header('Access-Controller-Allow-Origin', '*');
+//   res.header('Access-Controller-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   if(req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+//     return res.status(200).json({
+//       // header: res.header // to try later
+//     });
+//   }
+//   console.log("______cors_____")
+//   next();
+// });
+
 
 const server = http.createServer(app);
 const io = socketio(server)
@@ -24,7 +41,7 @@ io.on('connection', (socket) => {
       id: socket.id, name, room
     });
   console.log("___________addUser [%o]", addUser);
-      
+
     if(error) return callback(error);
     // emit from the back to the frontend
     socket.emit('message', {
@@ -53,14 +70,14 @@ io.on('connection', (socket) => {
   // we wait expected message on the back from front
   socket.on('sendMessage', () => {
     const user = getUser(socket.id);
-  
+
     io.to(user.room).emit('message', {
       user: user.name,
       text: message
     });
     callback();
   });
-  
+
   socket.on('disconnect', () => {
     console.log('user left');
   });
